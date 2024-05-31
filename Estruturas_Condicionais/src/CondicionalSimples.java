d2a2e288a39dea82253865e64091379177974066
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2024-05-31 20:49:16 -0300

    feat: Adicionar classe CondicionalSimples.java e atualizar .gitignore

---
+++ Estruturas_Condicionais/src/CondicionalSimples.java
@@ -0,0 +1,15 @@
+public class CondicionalSimples {
+
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.00;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+
+        
+        }
+        System.out.println("O seu saldo atual é: " + saldo);
+    }
+    
+}
