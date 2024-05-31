c7d1201889757039207a799f93b0ed56624e165a
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2024-05-31 20:56:37 -0300

    feat: Adicionar classe CondicionalComposta.java para estruturas condicionais

---
+++ Estruturas_Condicionais/src/CondicionalComposta.java
@@ -0,0 +1,15 @@
+public class CondicionalComposta {
+    public static void main(String[] args) {
+        // Exercicio nota escolar
+
+        int nota = 6;
+
+        if(nota >= 7){
+            System.out.println("Aprovado!");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+
+    }
+}
--- Estruturas_Condicionais/src/CondicionalSimples.java
+++ Estruturas_Condicionais/src/CondicionalSimples.java
@@ -1,15 +1,14 @@
 public class CondicionalSimples {
 
     public static void main(String[] args) {
+        // Exercicio de Caixa Eletronico
         double saldo = 25.0;
         double valorSolicitado = 17.00;
 
-        if(valorSolicitado < saldo){
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-
-        
         }
         System.out.println("O seu saldo atual é: " + saldo);
     }
-    
+
 }
