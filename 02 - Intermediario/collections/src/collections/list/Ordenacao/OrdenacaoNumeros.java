5430c2fd5ca54a694a1afa23f1874cf0602f4adb
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2024-06-12 16:13:00 -0300

    feat: Adicionar classe OrdenacaoNumeros para ordenar uma lista de números
    Adiciona a classe OrdenacaoNumeros ao diretório 02 - Intermediario/collections/src/collections/list/Ordenacao para implementar a funcionalidade de ordenar uma lista de números em ordem ascendente e descendente.

---
+++ 02 - Intermediario/collections/src/collections/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,78 @@
+package collections.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+	private List<Integer> numeros;
+
+	public OrdenacaoNumeros() {
+
+		this.numeros = new ArrayList<>();
+	}
+
+	public void adicionarNumero(int numero) {
+		numeros.add(numero);
+	}
+
+	public List<Integer> ordenarDescendente() {
+		List<Integer> numerosAscendente = new ArrayList<>(this.numeros);
+		if (!numeros.isEmpty()) {
+			numerosAscendente.sort(Collections.reverseOrder());
+			return numerosAscendente;
+		} else {
+			throw new RuntimeException("A lista está vazia!");
+		}
+
+	}
+
+	public List<Integer> ordenarAscendente() {
+		List<Integer> numerosAscendente = new ArrayList<>(this.numeros);
+		if (!numeros.isEmpty()) {
+			Collections.sort(numerosAscendente);
+			return numerosAscendente;
+		} else {
+			throw new RuntimeException("A lista está vazia!");
+		}
+
+	}
+	
+	public void exibirNumeros() {
+	    if (!numeros.isEmpty()) {
+	      System.out.println(this.numeros);
+	    } else {
+	      System.out.println("A lista está vazia!");
+	    }
+	  }
+
+	  public static void main(String[] args) {
+	    // Criando uma instância da classe OrdenacaoNumeros
+	    OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+	    // Adicionando números à lista
+	    numeros.adicionarNumero(2);
+	    numeros.adicionarNumero(5);
+	    numeros.adicionarNumero(4);
+	    numeros.adicionarNumero(1);
+	    numeros.adicionarNumero(99);
+
+	    // Exibindo a lista de números adicionados
+	    System.out.println("Exibindo a lista de números adicionados");
+	    numeros.exibirNumeros();
+
+	    // Ordenando e exibindo em ordem ascendente
+	    System.out.println("Ordenando e exibindo em ordem ascendente");
+	    System.out.println(numeros.ordenarAscendente());
+
+	    // Exibindo a lista
+	    System.out.println("Exibindo a lista");
+	    numeros.exibirNumeros();
+
+	    // Ordenando e exibindo em ordem descendente
+	    System.out.println("Ordenando e exibindo em ordem descendente");
+	    System.out.println(numeros.ordenarDescendente());
+
+	  }
+
+}
