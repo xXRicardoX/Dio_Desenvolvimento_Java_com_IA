3fa7f0248d98df306fe244b0f5aba4f8e79116e2
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2024-06-16 23:21:39 -0300

    feat: Refatorar código da classe Desafios
    Refatora o código da classe Desafios no diretório 02 - Intermediario/Exercicios/Exercicio Stream API Java/src/Exercicios/desafios para melhorar a legibilidade e organização do código.

--- 02 - Intermediario/Exercicios/Exercicio Stream API Java/src/Exercicios/Resposta.java
+++ 02 - Intermediario/Exercicios/Exercicio Stream API Java/src/Exercicios/Resposta.java
@@ -4,29 +4,32 @@ import Exercicios.desafios.Desafios;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.OptionalDouble;
 
 
 public class Resposta extends Desafios {
 
     public static void main(String[] args) {
 
-        Desafios tes = new Desafios();
 
-        List<Integer> numeros = Arrays.asList(3, 1, 4, 5, 7, 8, 10, 9, 2, 6);
+
+        List<Integer> numeros = Arrays.asList( 3, 1, 4, 5, 7, 8, 10, 9, 2, 6, 11, 12, 13, 14);
 
 
         // Desafio 1 - Mostre a lista na ordem numérica:
+        System.out.println("Desafio 01 - Mostre a lista na ordem numérica:");
         String ordenado = ordenarNumeroCrescente(numeros);
         System.out.println("Numero Ordenado\n" + ordenado);
 
         // Desafio 2 - Imprima a soma dos números pares da lista:
+        System.out.println("\nDesafio 02 - Imprima a soma dos números pares da lista:");
         String pares = numerosPares(numeros);
         System.out.println("Numeros Pares\n" + pares);
 
         // Desafio 3 - Verifique se todos os números da lista são positivos:
-        Boolean verificadorPositivo = verificadorNumerosPositivos(numeros);
-        System.out.println("Verificando se o numeros são positivos ou não");
+        boolean verificadorPositivo = verificadorNumerosPositivos(numeros);
+        System.out.println("\nDesafio 03 Verificando se o numeros são positivos ou não");
         if (verificadorPositivo) {
             System.out.println("Todos os numeros são positivo");
         } else {
@@ -34,11 +37,33 @@ public class Resposta extends Desafios {
         }
 
         //Desafio 4 - Remova todos os valores ímpares
-        System.out.println(" Removendo todos os valores ímpares");
-        String numerosPares = removerNumerosImpares(numeros);
+        System.out.println("\nDesafio 04 - Removendo todos os valores ímpares");
+        List<Integer> novaLista = removerNumerosImpares(numeros);
         System.out.println("Antiga Lista:\n" + numeros);
-        System.out.println("Nova Lista:\n" + numerosPares);
+        System.out.println("Nova Lista:\n" + novaLista);
 
+
+        // Desafio 5 - Calcule a média dos números maiores que 5
+        System.out.println("\nDesafio 05 - Calcule a média dos números maiores que 5");
+        OptionalDouble mediaMaiorQueCinco = mediaMaiorQueCinco(numeros);
+        System.out.println("A média maior que cinco é " + mediaMaiorQueCinco);
+
+        // Desafio 6 - Verificar se a lista contém algum número maior que 10:
+        System.out.println("Desafio 6 - Verificar se a lista contém algum número maior que 10:");
+        boolean verificadorMaiorDez = verificarNumeroMaiorQueDez(numeros);
+        if(verificadorMaiorDez){
+            System.out.println("Tem numero maior que dez");
+        }else {
+            System.out.println("NÂO tem numero maior que dez");
+        }
+
+        // Desafio 7 - Encontrar o segundo número maior da lista
+        System.out.println("\nDesafio 07 - Encontrar o segundo número maior da lista");
+        Optional<Integer> encontrandoSegundoMaiorValor = verificarSegundoMaiorNumero(numeros);
+        encontrandoSegundoMaiorValor.ifPresentOrElse(
+                n -> System.out.println("O segundo maio numero da lista é: " + n),
+                () -> System.out.println("A lista não tem um segundo maior numero")
+        );
 
     }
 }
--- 02 - Intermediario/Exercicios/Exercicio Stream API Java/src/Exercicios/desafios/Desafios.java
+++ 02 - Intermediario/Exercicios/Exercicio Stream API Java/src/Exercicios/desafios/Desafios.java
@@ -1,11 +1,10 @@
 package Exercicios.desafios;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Desafios {
+    
     /**
      * Desafio 1 - Mostre a lista na ordem numérica:
      * Crie um programa que utilize a Stream API para ordenar a lista de números em ordem crescente e a exiba no console.
@@ -43,13 +42,43 @@ public class Desafios {
      * Desafio 4 - Remova todos os valores ímpares:
      * Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
      */
-    public static String removerNumerosImpares(List<Integer> numeros) {
+    public static List<Integer> removerNumerosImpares(List<Integer> numeros) {
+        List<Integer> listaModificavel = new ArrayList<>(numeros);
+        listaModificavel.removeIf(n -> n % 2 != 0);
+        return listaModificavel;
+    }
+
+    /**
+     * Desafio 5 - Calcule a média dos números maiores que 5:
+     * Com a Stream API, calcule a média dos números maiores que 5 e exiba o resultado no console.
+     */
+    public static OptionalDouble mediaMaiorQueCinco(List<Integer> numeros) {
         return numeros.stream()
-                .filter(n -> n % 2 == 0)
-                .map(String::valueOf) // Converter cada numero em String
-                .collect(Collectors.joining(", "));// Converter cada numero em String
+                .filter(n -> n > 5)
+                .mapToInt(Integer::intValue)
+                .average();
+    }
 
+    /**
+     * Desafio 6 - Verificar se a lista contém algum número maior que 10:
+     * Utilize a Stream API para verificar se a lista contém algum número maior que 10 e exiba o resultado no console.
+     */
+    public static boolean verificarNumeroMaiorQueDez(List<Integer> numeros) {
 
+        return numeros.stream()
+                .anyMatch(n -> n > 10);
+    }
+
+    /**
+     * Desafio 7 - Encontrar o segundo número maior da lista:
+     * Com a ajuda da Stream API, encontre o segundo número maior da lista e exiba o resultado no console.
+     */
+    public static Optional<Integer> verificarSegundoMaiorNumero(List<Integer> numeros) {
+        return numeros.stream()
+                .distinct() // Remove duplicadas
+                .sorted(Comparator.reverseOrder())
+                .skip(1) // Pula o maior numero
+                .findFirst(); // Obtem o segundo maior número
 
     }
 
